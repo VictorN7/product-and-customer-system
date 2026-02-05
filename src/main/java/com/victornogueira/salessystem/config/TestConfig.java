aca8e9922703953585b56d2accf2220484c7bd40
VictorN7 <victorhnogueira@hotmail.com>
2026-02-05 00:52:12 -0300

    feat: The manyToMany association with the product and category entities

--- src/main/java/com/victornogueira/salessystem/config/TestConfig.java
+++ src/main/java/com/victornogueira/salessystem/config/TestConfig.java
@@ -60,9 +60,22 @@ public class TestConfig implements CommandLineRunner{
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, ""); 
 		
-		userRepository.saveAll(Arrays.asList(u1,u2,u3,u4,u5)); // Instancia uma lista de usuários
+		// Instancia uma lista de usuários
+		
+		userRepository.saveAll(Arrays.asList(u1,u2,u3,u4,u5)); 
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3,o4,o5));
 		categoryRepository.saveAll(Arrays.asList(c1,c2,c3));
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
+		// Relação categorias com produtos
+		
+		p1.getCategories().add(c2); 
+		p2.getCategories().add(c1);
+		p2.getCategories().add(c3);
+		p3.getCategories().add(c3);
+		p4.getCategories().add(c3);
+		p5.getCategories().add(c2);
+
 		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 	} 
 }
