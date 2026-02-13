96658fb80f01240b910836b5271eeebe4ad1e271
VictorN7 <victorhnogueira@hotmail.com>
2026-02-13 13:14:31 -0300

    feat: Create the exception handler for findbyId

--- src/main/java/com/victornogueira/salessystem/services/UserService.java
+++ src/main/java/com/victornogueira/salessystem/services/UserService.java
@@ -1,7 +1,6 @@
 package com.victornogueira.salessystem.services;
 
 import java.util.List;
-import java.util.Optional;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.victornogueira.salessystem.entities.User;
 import com.victornogueira.salessystem.repositories.UserRepository;
+import com.victornogueira.salessystem.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -20,10 +20,9 @@ public class UserService {
 		return userRepository.findAll();
 	}
 
-	public User findById(Long Id) {
+	public User findById(Long id) {
 		
-		Optional<User> user = userRepository.findById(Id);
-		return user.get();
+		return userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insertUser(User user) {
